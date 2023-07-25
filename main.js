8eb617fbf29eb1e182df5cbe271400a26368368b
Pradeep Kumar Gupta <100694988+Pradeepgupta77@users.noreply.github.com>
2023-07-26 00:36:58 +0530

    commit
    26/07/2023

--- main.js
+++ main.js
@@ -15,21 +15,84 @@ document.getElementById("downloadLink").addEventListener("click", function () {
     link.download = "pradeepGupta.pdf"; // Set the downloaded file name
     link.click(); // Programmatically trigger the click event on the anchor element
 });
-
+// asing veriabele for avery page and div 
+//home page  
 const mainDiv = document.querySelector(".main");
+// all mini page section 
 const pageDiv = document.querySelector(".pages");
+// nav bar in side page 
+const navLogo = document.querySelector(".nav-logo")
+const navBar = document.querySelector(".panel")
+// portfolio
+const portfolioMainDiv = document.querySelector(".portfolio-main");
+//  portfolio page div
+const webdiv = document.querySelector(".web-div");
+const animetiondiv = document.querySelector(".animetion-div");
+const reactDiv = document.querySelector(".react-div");
+const wordpressDiv = document.querySelector(".Wordpress-div");
+// about 
+const aboutMainDiv = document.querySelector(".about-main");
+// services  
+const servicesMainDiv = document.querySelector(".Service-main");
+// contact
+const contactMainDiv = document.querySelector(".contact-main");
 
-// hero main div
-const heroMainDiv = document.querySelector(".hero-main")
+
+// nav bar in side page js 
+const navLogoButton = document.getElementById("nav-logo-btn")
+const navCloseButton = document.getElementById("close-nav")
+const gotoAboutButton = document.querySelector(".about-button");
+const gotoPortfolioButton = document.querySelector(".portfolio-button");
+const gotoServiceButton = document.querySelector(".service-button");
+const gotoContactButton = document.querySelector(".contact-button");
+
+// open nav bar 
+navLogoButton.addEventListener("click", function () {
+    navLogo.classList.add("d-non");
+    navBar.classList.remove("d-non");
+})
+// close nav bar 
+navCloseButton.addEventListener("click", function () {
+    navLogo.classList.remove("d-non");
+    navBar.classList.add("d-non");
+})
+
+//go to about page 
+gotoAboutButton.addEventListener("click", function () {
+    closePgeDiv()
+    pageDiv.classList.remove("d-non");
+    aboutMainDiv.classList.remove("d-non");
+})
+//go to portfolio page 
+gotoPortfolioButton.addEventListener("click", function () {
+    closePgeDiv()
+    pageDiv.classList.remove("d-non");
+    portfolioMainDiv.classList.remove("d-non");
+})
+//go to service page 
+gotoServiceButton.addEventListener("click", function () {
+    closePgeDiv()
+    pageDiv.classList.remove("d-non");
+    servicesMainDiv.classList.remove("d-non");
+})
+//go to contact page 
+gotoContactButton.addEventListener("click", function () {
+    closePgeDiv()
+    pageDiv.classList.remove("d-non");
+    contactMainDiv.classList.remove("d-non");
+})
+
+
+
 
 // portfolio section js 
-
-const portfolioMainDiv = document.querySelector(".portfolio-main");
+// home page button go to portfolio 
+const gotoPortfolio = document.getElementById("goto-portfolio");
 // home portfolio section button 
 const webButton = document.getElementById("webButton");
 const animetionButton = document.getElementById("animetionButton");
 const wordpressButton = document.getElementById("wordpressButton");
-const gotoPortfolio = document.querySelector(".goto-portfolio");
+
 
 //  portfolio page button
 const pWebButton = document.getElementById("p-webButton");
@@ -37,11 +100,7 @@ const pAnimetionButton = document.getElementById("p-animetionButton");
 const pReactButton = document.getElementById("p-reactButton");
 const pWordpressButton = document.getElementById("p-wordpressButton");
 
-//  portfolio page div
-const webdiv = document.querySelector(".web-div");
-const animetiondiv = document.querySelector(".animetion-div");
-const reactDiv = document.querySelector(".react-div");
-const wordpressDiv = document.querySelector(".Wordpress-div");
+
 
 
 // home portfolio section js 
@@ -101,7 +160,6 @@ pWordpressButton.addEventListener("click", function () {
 })
 
 // About section js 
-const aboutMainDiv = document.querySelector(".about-main")
 const gotoAbout = document.getElementById("goto-about")
 
 gotoAbout.addEventListener("click", function () {
@@ -111,7 +169,6 @@ gotoAbout.addEventListener("click", function () {
 })
 
 // services section js 
-const servicesMainDiv = document.querySelector(".Service-main")
 const gotoServices = document.getElementById("goto-services")
 gotoServices.addEventListener("click", function () {
     mainDiv.classList.add("d-non");
@@ -120,27 +177,39 @@ gotoServices.addEventListener("click", function () {
 })
 
 // Contact section js 
-const contactMainDiv = document.querySelector(".contact-main")
+
 const gotoContact = document.getElementById("goto-Contact")
 gotoContact.addEventListener("click", function () {
-    mainDiv.classList.add("d-non")
-    pageDiv.classList.remove("d-non")
-    contactMainDiv.classList.remove("d-non")
+    mainDiv.classList.add("d-non");
+    pageDiv.classList.remove("d-non");
+    contactMainDiv.classList.remove("d-non");
 
 })
 
 
 // pages
 
-// go to home 
-const gotoHome = document.getElementById("goto-home")
-gotoHome.addEventListener("click", function () {
-    pageDiv.classList.add("d-non");
+// go to home
+const homeButtons = document.querySelectorAll(".home-button");
+function goToHomePage() {
     mainDiv.classList.remove("d-non");
-    heroMainDiv.classList.add("d-non");
+    closePgeDiv()
+    mainDiv.classList.remove("d-non");
+
+}
+
+homeButtons.forEach(button => {
+    button.addEventListener("click", goToHomePage);
+});
+
+
+// close page div 
+function closePgeDiv() {
+    pageDiv.classList.add("d-non");
+    navLogo.classList.remove("d-non");
+    navBar.classList.add("d-non");
     portfolioMainDiv.classList.add("d-non");
     aboutMainDiv.classList.add("d-non");
     servicesMainDiv.classList.add("d-non");
     contactMainDiv.classList.add("d-non");
-
-})
+}
